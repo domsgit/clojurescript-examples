ed28352f8cb12999b614c05989287534187a57c2
domsgit <domsgit@outlook.com>
2020-01-13 08:11:34 +0800

    add react_deps“

---
+++ react_deps/src/reactdeps/core.cljs
@@ -0,0 +1,7 @@
+(ns reactdeps.core
+  (:require react-dom))
+
+(.render js/ReactDOM
+  (.createElement js/React "h2" nil "Hello, React!")
+  (.getElementById js/document "app"))
+
---
+++ react_deps/start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+clj -m cljs.main -c reactdeps.core -r
